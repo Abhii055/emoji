3ef0dd40ea516e317b84388a4b502ea98cbff5d9
Abhinav <abhinavdwivedi0807@gmail.com>
2023-12-27 13:30:19 +0530

    shadow_commit

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 import  Dictionary from "./dictionary";
 import FormCondition from './formcondition';
 import Cal from './Cal';
-import Strike from './Cal'
+
 
 function App(){
     return(
     <div>
     <Dictionary />
-    <Strike />
     <FormCondition />
     <Cal />
-    
     </div>
     )
 }
